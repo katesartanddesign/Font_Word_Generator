c15f3245e4d1f82d0df90e865af1b397d7d2ee38
katesartanddesign <katesartanddesign@gmail.com>
2026-01-11 21:42:26 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/font-word-generator/'
 })
+```
+
+The key part is `base: '/font-word-generator/'` - this tells Vite where the app will be hosted.
+
+**Save it (Cmd + S)**
+
+**Then in GitHub Desktop:**
+1. Commit message: `Fix base path for GitHub Pages`
+2. Click **Commit to main**
+3. Click **Push origin**
+
+**Wait for the yellow dot to turn green again (2-3 minutes)**
+
+Then try the URL:
+```
+https://katesartanddesign.github.io/font-word-generator/
